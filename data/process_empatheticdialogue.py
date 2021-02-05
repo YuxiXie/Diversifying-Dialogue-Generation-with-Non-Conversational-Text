d2ccd7bab73b2e5a7ef47870cbfa52c9286df579
YuxiXie <xieyuxi@pku.edu.cn>
2021-02-05 15:59:17 +0800

    update

--- data/process_empatheticdialogue.py
+++ data/process_empatheticdialogue.py
@@ -8,28 +8,41 @@ json_load = lambda x: json.load(codecs.open(x, 'r', encoding='utf-8'))
 json_dump = lambda d, p: json.dump(d, codecs.open(p, 'w', 'utf-8'), indent=2, ensure_ascii=False)
 
 def get_samples(filename):
+    samples, avg_length = {}, 0
+    
     with open(filename, 'r', encoding='utf-8') as f:
         data = csv.reader(f)
         header = next(data)
 
-    samples, avg_length = {}, 0
-    for row in tqdm(data):
-        conv_id = row[0]
-        if conv_id not in samples:
-            samples[conv_id] = {'context': row[2], 'length': 0, 'prompt': row[3], 'utterances': []}
-        samples[conv_id]['utterances'].append([row[1], row[4], row[5], row[6]])
+        for row in tqdm(data):
+            conv_id = row[0]
+            if conv_id not in samples:
+                samples[conv_id] = {'context': row[2], 'length': 0, 'prompt': row[3], 'content': []}
+            samples[conv_id]['content'].append([row[1], row[4], row[5]])
     
+    data = []
     for k, v in tqdm(samples.items()):
-        v['utterances'].sort(key=lambda x: x[0])
-        raw_id, is_available = v['utterances'][0][1], True
-        for x in v['utterances']:
+        v['content'].sort(key=lambda x: x[0])
+        raw_id, is_available = v['content'][0][1], True
+        for x in v['content'][1:]:
             if x[1] == raw_id:
-                flag = False
+                is_available = False
                 break
             raw_id = x[1]
+        
+        if is_available:
+            v['content'] = [x[2] for x in v['content']]
+            v['length'] = len(v['content'])
+            if v['length'] > 1:
+                data.append(v)
+    
+    samples = data
+    samples.sort(key=lambda x: x['length'])
+    return samples
 
-with open(sys.argv[1]) as f:
-    f_csv = csv.reader(f)
-    headers = next(f_csv)
-    for row in f_csv:
-        import ipdb; ipdb.set_trace()
+
+if __name__ == '__main__':
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    json_dump(get_samples(input_file), output_file)
